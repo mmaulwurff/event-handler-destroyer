5c0201b80e52a0f3acc261eb7a7ec389657eb031
Alexander Kromm <mmaulwurff@gmail.com>
2021-04-03 22:44:35 +0700

    add version to build

--- build.sh
+++ build.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-#filename=build/event-handler-destroyer-$(git describe --abbrev=0 --tags).pk3
-filename=build/event-handler-destroyer.pk3
+mkdir -p build
+
+filename=build/event-handler-destroyer-$(git describe --abbrev=0 --tags).pk3
 
 rm  -f  "$filename"
 zip -R0 "$filename" "*.md" "*.txt" "*.zs"
